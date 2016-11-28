9eb22c2773e7d007d816b0a967967057b351b779
Christian Mauricio Chacua Delgado <cchacua@gmail.com>
2016-11-28 10:11:19 +0100

    28112016 1011

---
+++ main.R
@@ -0,0 +1,14 @@
+# Institution: Université de Bordeaux
+# Subject: Mémoire M1- Intelligence économique
+# Authors: 
+# Vianney CHARPENTIER
+# Christian Mauricio CHACUA DELGADO
+
+
+wiot.files<-list.files(path="../data/wiot", full.names=TRUE)
+wiot.files
+
+# Install packages
+source("./scripts/packages-install.R")
+# Load packages
+source("./scripts/packages-load.R")
