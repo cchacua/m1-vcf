6c62009a56e08dbe28e5ba08c11dd83dcd7f1ef3
Christian Mauricio Chacua Delgado <cchacua@gmail.com>
2017-03-07 22:19:06 +0100

    07032017 2218

--- main.R
+++ main.R
@@ -1,14 +1,21 @@
 # Institution: Université de Bordeaux
 # Subject: Mémoire M1- Intelligence économique
-# Authors: 
-# Vianney CHARPENTIER
-# Christian Mauricio CHACUA DELGADO
+# Author: Christian Mauricio CHACUA DELGADO
+
+memory.size(max = TRUE)
+options(digits=4)
+options(stringsAsFactors=FALSE)
 
 
 wiot.files<-list.files(path="../data/wiot", full.names=TRUE)
 wiot.files
+wiot.2000<-load(wiot.files[1])
+
+
 
 # Install packages
 source("./scripts/packages-install.R")
 # Load packages
 source("./scripts/packages-load.R")
+
+
---
+++ scripts/packages-install.R
@@ -0,0 +1,13 @@
+install.packages("igraph")
+install.packages("R.utils")
+install.packages("ggplot2")
+install.packages("tcltk")
+install.packages("extrafont")
+font_install("fontcm")
+install.packages("data.table")
+install.packages("RColorBrewer")
+install.packages('network')
+install.packages('ndtv', dependencies=T)
+# http://curleylab.psych.columbia.edu/netviz/netviz1.html#/51
+# http://kateto.net/network-visualization
+# http://www.kateto.net/polnet2015
