4931afff91088ee057af59aa0f0563a0165d7031
Christian Mauricio Chacua Delgado <cchacua@gmail.com>
2017-03-09 21:16:46 +0100

    09032017 2116

--- main.R
+++ main.R
@@ -23,20 +23,22 @@ source("./scripts/packages-load.R")
   wiot.df<-as.data.frame(wiot[1])
   wiot.colnames<-as.data.frame(colnames(wiot.df))
   # CI data goes until line 2464= 44 countries * 56 sectors
-  wiot.df<-wiot.df[1:2464,6:2470]
+  wiot.df.ci<-wiot.df[1:2464,6:2469]
+  # Columns 6:2469 have the CI data
+  # Columns 57-61 for each country goes from colums 2470:2689
+    # CONS_h,	CONS_np,	CONS_g,	GFCF,	INVEN,	EXP	GO
+    # Final consumption expenditure by households	Final consumption expenditure by non-profit organisations serving households (NPISH)	Final consumption expenditure by government	Gross fixed capital formation	Changes in inventories and valuables	Exports	Total output
+  # Colum 2690 has the total production, which is the same value as in line 2472 (GO)
+    # View(wiot.df[1,2690])
+    # View(wiot.df[2472,6])
+  wiot.net.ci<- network(wiot.df.ci, matrix.type="adjacency", directed=TRUE, loops=TRUE, multiple=F, ignore.eval = F)
+  #?network
   
   
 # Network with in terms of technical coefficients
   wiot.df<-as.data.frame(wiot[1])
   # CI data goes until line 2464= 44 countries * 56 sectors
   # Line 2472 (GO) has the Output at basic prices
-  wiot.df<-wiot.df[1:2464,6:2470]
-  
-
-
-wiot.2000.adj<- wiot.2000
-wiot.2000.net <- network(wiot.2000.adj, matrix.type="adjacency", directed=TRUE,
-                loops=F, multiple=F, ignore.eval = F)
-
-
-
+  wiot.df.ci<-wiot.df[c(1:2464, 2472),6:2469]
+  wiot.df.cin<-sapply(wiot.df.ci, dividelast)
+  wiot.df.cin<-as.data.frame(wiot.df.cin)
--- scripts/packages-load.R
+++ scripts/packages-load.R
@@ -1,4 +1,14 @@
 library('ndtv')
 library('network')
 
+# Custom Functions
+
 open.rdata<-function(x){local(get(load(x)))}
+
+dividelast<-function(column){
+  #column<-wiot.df.ci[,1]
+  vector<-column[1:length(column)-1]
+  scalar<-column[length(column)]
+  output<-vector/scalar
+  output<-as.data.frame(output)
+}
